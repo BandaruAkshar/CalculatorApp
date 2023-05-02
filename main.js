52cb53b66f5af829dfbc95f138151b7d17b036c2
BandaruAkshar <bandaru.akshar2021@vitstudent.ac.in>
2023-05-02 23:46:51 +0530

    First Attempt

---
+++ main.js
@@ -0,0 +1,27 @@
+let answer = document.getElementById("answer");
+
+inputs = document.querySelectorAll(".buttons input");
+
+inputs.forEach(ip => {
+    ip.addEventListener("click",(e) =>{
+        txt= e.target.value;    
+        if(txt == "AC")
+        {
+            answer.value= '';
+        }
+        else if(txt == "=")
+        {
+            answer.value=eval(answer.value);
+        }
+        else if(txt == "DEL")
+        {
+            answer.value= answer.value.substr(0,answer.value.length-1);
+        }
+        else
+        {
+            answer.value+=txt;
+        }
+
+    });
+
+});
